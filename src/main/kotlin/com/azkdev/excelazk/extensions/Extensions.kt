db98b0167036226ee1562d0d1d2ddd513c0fd4bb
azkdev <azkdev@outlook.com>
2018-02-16 01:34:27 +0200

    Extensions functions for Row, Sheet.

--- src/main/kotlin/com/azkdev/excelazk/extensions/Extensions.kt
+++ src/main/kotlin/com/azkdev/excelazk/extensions/Extensions.kt
@@ -1,2 +1,28 @@
 package com.azkdev.excelazk.extensions
 
+import org.apache.poi.ss.usermodel.Cell
+import org.apache.poi.ss.usermodel.Sheet
+import org.apache.poi.ss.usermodel.Row
+
+operator fun Sheet.get(n: Int): Row {
+    return this.getRow(n) ?: this.createRow(n)
+}
+
+operator fun Sheet.get(x: Int, y: Int): Cell {
+    val row: Row = this[y]
+    return row[x]
+}
+
+operator fun Row.get(n: Int): Cell {
+    return this.getCell(n) ?: this.createCell(n)
+}
+
+operator fun Sheet.set(x: Int, y: Int, value: Any) {
+    val cell: Cell = this[x, y]
+    when (value) {
+        is String -> cell.setCellValue(value)
+        is Int -> cell.setCellValue(value.toDouble())
+        is Double -> cell.setCellValue(value)
+        else -> throw IllegalArgumentException("Illegal argument!")
+    }
+}
